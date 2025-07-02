3751ef1c0da385a254e56aae7547394d094c4afe
TOUAZI AIMEN RYAD <touaziriad94@gmail.com>
2025-07-02 16:12:09 +0200

    docker compose fix

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Hello from React!</h1>
-      <p>This is addddddddd c React frontend.</p>
+      <p>This is adddddgggggggend.</p>
     </div>
   );
 }
